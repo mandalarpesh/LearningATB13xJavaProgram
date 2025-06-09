a925b7cdfa0430900d4cc48a44bc1d20bc926abc
AMandal <mandalarpesh@gmail.com>
2025-06-09 11:21:41 +0530

    9th June 2025

---
+++ src/ex_01_Java_Basic/Lab003_NoMainMethod.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab003_NoMainMethod {
+
+
+
+
+}
---
+++ src/ex_01_Java_Basic/Lab008_Comments.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab008_Comments {
+    public static void main(String[] args) {
+        /**
+         * Author - Arpesh Mandal
+         * Course :  ATB13x
+         * This is a documentation comment
+         * @param args command-line arguments
+         * @return void
+         */
+            // Method implementation
+    }
+}
