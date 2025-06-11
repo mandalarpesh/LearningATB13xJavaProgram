501a42c03f2ca468008dac884d8f11354bb40e41
AMandal <mandalarpesh@gmail.com>
2025-06-11 12:35:07 +0530

    11th June 2025

---
+++ src/ex_04_Operators/Lab050_New_Operator.java
@@ -0,0 +1,10 @@
+package ex_04_Operators;
+
+public class Lab050_New_Operator {
+    public static void main(String[] args) {
+        // New Operator - oops
+        String name =  new String("Arpesh");
+        // Learn this in OOPs
+
+    }
+}
---
+++ src/ex_04_Operators/Lab052_Interview.java
@@ -0,0 +1,13 @@
+package ex_04_Operators;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Lab052_Interview {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b = (10 == 11); // == -> Comparison 2 values
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
