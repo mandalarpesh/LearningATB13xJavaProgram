df6ba601c3de697297d59381c0c49c5230d636ff
AMandal <mandalarpesh@gmail.com>
2025-06-17 12:22:07 +0530

    17th June 2025 Task

---
+++ src/ex_11_While/Lab121_While_IQ_Factorial.java
@@ -0,0 +1,32 @@
+package ex_11_While;
+
+import java.util.Scanner;
+
+public class Lab121_While_IQ_Factorial {
+    public static void main(String[] args) {
+        // Factorial Program
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Factorial Program\nEnter the number!");
+        if (!sc.hasNextInt()) {
+            System.out.println("Enter Integer you fool!");
+            return;
+        }
+        int number = sc.nextInt();
+        long factorial = 1;
+
+        if(number < 0){
+            System.out.println("Negative Factorial is not allowed!");
+            return;
+        }
+        if(number <= 0){
+            System.out.println(factorial);
+        }else{
+            // Calculate the factorial
+            for (int i = 1; i <= number ; i++) {
+                factorial = factorial*i;
+            }
+        }
+        System.out.println("Factorial is -> " + factorial); // Print the calculated factorial
+
+    }
+}
